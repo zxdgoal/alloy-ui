1e7108d067fad8088d57b91274128bf8b3b04a04
John Zhao <john.zhao@liferay.com>
2016-03-01 14:41:05 +0800

    AUI-757 Cannot drag and a child node if parent tree is not expanded when the tree is first rendered

--- src/aui-tree/js/aui-tree-node.js
+++ src/aui-tree/js/aui-tree-node.js
@@ -1162,6 +1162,8 @@ var TreeNodeIO = A.Component.create(
 			ioSuccessHandler: function() {
 				var instance = this;
 				var io = instance.get(IO);
+				var ownerTree = instance.get(OWNER_TREE);
+
 				var args = Array.prototype.slice.call(arguments);
 				var length = args.length;
 
@@ -1187,6 +1189,10 @@ var TreeNodeIO = A.Component.create(
 				instance.createNodes(nodes);
 
 				instance.expand();
+
+				if (ownerTree && ownerTree.ddDelegate) {
+					ownerTree.ddDelegate.syncTargets();
+				}
 			},
 
 			/**
