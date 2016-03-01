f26ca1b4f022f3d94e0d77403488c321e1471210
John Zhao <john.zhao@liferay.com>
2016-03-01 14:38:18 +0800

    AUI-795 - TreeView - Add the ability to exclude nodes from being draggable

--- src/aui-tree/js/aui-tree-node.js
+++ src/aui-tree/js/aui-tree-node.js
@@ -18,6 +18,7 @@ var L = A.Lang,
 	CONTAINER = 'container',
 	CONTENT = 'content',
 	CONTENT_BOX = 'contentBox',
+	DRAGGABLE = 'draggable',
 	EXPANDED = 'expanded',
 	HELPER = 'helper',
 	HIDDEN = 'hidden',
@@ -25,6 +26,7 @@ var L = A.Lang,
 	HITAREA = 'hitarea',
 	ICON = 'icon',
 	ICON_EL = 'iconEl',
+	INVALID = 'invalid',
 	ID = 'id',
 	LABEL = 'label',
 	LABEL_EL = 'labelEl',
@@ -66,6 +68,7 @@ var L = A.Lang,
 	CSS_TREE_LABEL = getCN(TREE, LABEL),
 	CSS_TREE_NODE = getCN(TREE, NODE),
 	CSS_TREE_NODE_CONTENT = getCN(TREE, NODE, CONTENT),
+	CSS_TREE_NODE_CONTENT_INVALID = getCN(TREE, NODE, CONTENT, INVALID),
 	CSS_TREE_NODE_HIDDEN_HITAREA = getCN(TREE, NODE, HIDDEN, HITAREA),
 	CSS_TREE_NODE_LEAF = getCN(TREE, NODE, LEAF),
 	CSS_TREE_NODE_OVER = getCN(TREE, NODE, OVER),
@@ -356,6 +359,7 @@ var TreeNode = A.Component.create(
 				// Sync the Widget TreeNode id with the BOUNDING_BOX id
 				instance._syncTreeNodeBBId();
 
+				instance._uiSetDraggable(instance.get(DRAGGABLE));
 				instance._uiSetExpanded(instance.get(EXPANDED));
 				instance._uiSetLeaf(instance.get(LEAF));
 			},
@@ -370,6 +374,7 @@ var TreeNode = A.Component.create(
 				var instance = this;
 
 				instance.after('childrenChange', A.bind(instance._afterSetChildren, instance));
+				instance.after('draggableChange', A.bind(instance._afterDraggableChange, instance));
 				instance.after('expandedChange', A.bind(instance._afterExpandedChange, instance));
 				instance.after('idChange', instance._afterSetId, instance);
 				instance.after('leafChange', A.bind(instance._afterLeafChange, instance));
@@ -718,6 +723,20 @@ var TreeNode = A.Component.create(
 				instance.fire('unselect');
 			},
 
+			/**
+			 * Fire after draggable change.
+			 *
+			 * @method _afterDraggableChange
+			 * @param {EventFacade} event
+			 * @protected
+			 */
+			_afterDraggableChange: function(event) {
+				var instance = this;
+
+				instance._uiSetDraggable(event.newVal);
+				instance.syncUI();
+			},
+
 			/*
 			* Fires when <code>mouseover</code> the current TreeNode.
 			*
@@ -788,6 +807,14 @@ var TreeNode = A.Component.create(
 				}
 
 				return sibling;
+			},
+
+			_uiSetDraggable: function(val) {
+				var instance = this;
+
+				var contentBox = instance.get(CONTENT_BOX);
+
+				contentBox.toggleClass(CSS_TREE_NODE_CONTENT_INVALID, !val);
 			},
 
 			_uiSetExpanded: function(val) {
